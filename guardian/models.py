c1cb4be3020577c2f7913f8918410389d0e6e1b3
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-10-25 23:40:50 +0800

    Update models.py

--- guardian/models.py
+++ guardian/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from teacher.models import TeacherProfile
 
-# Create your models here.
+class Guardian(models.Model):
+    teacher = models.ForeignKey(TeacherProfile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.TextField()
+    relationship = models.CharField(max_length=50)
+    contact = models.CharField(max_length=15)
+    student_name = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.student_name}"
