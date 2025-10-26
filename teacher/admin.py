8580cff86b94371e563132c5360d8f8cecbc1150
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-10-26 19:28:50 +0800

    Update admin.py

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TeacherProfile, Attendance
 
-# Register your models here.
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'age', 'gender', 'section', 'contact']
+    search_fields = ['user__username', 'section', 'contact']
+    list_filter = ['gender', 'section']
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['student_name', 'teacher', 'timestamp']
+    search_fields = ['student_name', 'qr_code_data']
+    list_filter = ['timestamp', 'teacher']
+    readonly_fields = ['timestamp']
