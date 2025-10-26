1a4ae4f65c8d061f25fbb40f81cbff6f1f12692d
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-10-26 19:30:53 +0800

    Update admin.py

--- guardian/admin.py
+++ guardian/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Guardian
 
-admin.site.register(Guardian)
+@admin.register(Guardian)
+class GuardianAdmin(admin.ModelAdmin):
+    list_display = ['name', 'student_name', 'relationship', 'contact', 'teacher', 'timestamp']
+    search_fields = ['name', 'student_name', 'contact']
+    list_filter = ['relationship', 'timestamp', 'teacher']
+    readonly_fields = ['timestamp']
+    
+    fieldsets = (
+        ('Guardian Information', {
+            'fields': ('name', 'age', 'address', 'relationship', 'contact')
+        }),
+        ('Student & Teacher', {
+            'fields': ('student_name', 'teacher', 'timestamp')
+        }),
+    )
