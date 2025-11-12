b1d753d96ffa190d2b2b3dc39aee9eff8bee7185
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-12 10:01:03 +0800

    Update admin.py

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TeacherProfile, Attendance
+from .models import TeacherProfile, Attendance, Absence, Dropout, UnauthorizedPerson
 
 @admin.register(TeacherProfile)
 class TeacherProfileAdmin(admin.ModelAdmin):
@@ -9,7 +9,29 @@ class TeacherProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ['student_name', 'teacher', 'timestamp']
-    search_fields = ['student_name', 'qr_code_data']
-    list_filter = ['timestamp', 'teacher']
+    list_display = ['student_name', 'student_lrn', 'teacher', 'date', 'session', 'status', 'timestamp']
+    search_fields = ['student_name', 'student_lrn', 'qr_code_data']
+    list_filter = ['date', 'status', 'session', 'teacher']
+    readonly_fields = ['timestamp']
+    ordering = ['-date', '-timestamp']
+
+@admin.register(Absence)
+class AbsenceAdmin(admin.ModelAdmin):
+    list_display = ['student_name', 'teacher', 'date', 'reason', 'timestamp']
+    search_fields = ['student_name', 'reason']
+    list_filter = ['date', 'teacher']
+    readonly_fields = ['timestamp']
+
+@admin.register(Dropout)
+class DropoutAdmin(admin.ModelAdmin):
+    list_display = ['student_name', 'teacher', 'date', 'reason', 'timestamp']
+    search_fields = ['student_name', 'reason']
+    list_filter = ['date', 'teacher']
+    readonly_fields = ['timestamp']
+
+@admin.register(UnauthorizedPerson)
+class UnauthorizedPersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'student_name', 'guardian_name', 'relation', 'contact', 'timestamp']
+    search_fields = ['name', 'student_name', 'guardian_name', 'contact']
+    list_filter = ['relation', 'teacher', 'timestamp']
     readonly_fields = ['timestamp']
