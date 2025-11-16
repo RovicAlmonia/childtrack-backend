458d051332614e8e0c55d3c24a9d16b4c109e878
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-16 18:14:01 +0800

    Update admin.py

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TeacherProfile, Attendance, Dropout, UnauthorizedPerson
+from .models import TeacherProfile, Attendance, UnauthorizedPerson
 
 @admin.register(TeacherProfile)
 class TeacherProfileAdmin(admin.ModelAdmin):
@@ -12,29 +12,6 @@ class AttendanceAdmin(admin.ModelAdmin):
     list_display = ['student_name', 'teacher', 'date', 'status', 'session', 'gender', 'timestamp']
     search_fields = ['student_name', 'student_lrn', 'teacher__user__username']
     list_filter = ['status', 'date', 'session', 'gender', 'teacher']
-    date_hierarchy = 'date'
-    ordering = ['-date', '-timestamp']
-    
-    def save_model(self, request, obj, form, change):
-        if change and 'status' in form.changed_data:
-            if obj.status == 'Dropped Out':
-                Dropout.objects.create(
-                    teacher=obj.teacher,
-                    student_name=obj.student_name,
-                    student_lrn=obj.student_lrn,
-                    gender=obj.gender,
-                    date=obj.date,
-                    reason='Changed to dropout from admin panel'
-                )
-                obj.delete()
-                return
-        super().save_model(request, obj, form, change)
-
-@admin.register(Dropout)
-class DropoutAdmin(admin.ModelAdmin):
-    list_display = ['student_name', 'teacher', 'date', 'reason', 'gender', 'timestamp']
-    search_fields = ['student_name', 'student_lrn', 'teacher__user__username', 'reason']
-    list_filter = ['date', 'teacher', 'gender']
     date_hierarchy = 'date'
     ordering = ['-date', '-timestamp']
 
