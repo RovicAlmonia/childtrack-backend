be9570f064c38ba1a24ea9090fecf8d03fd78a05
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-16 18:21:53 +0800

    Update settings.py

--- childtrack_project/settings.py
+++ childtrack_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': dj_database_url.parse(
         os.getenv(
             'DATABASE_URL',
-            'postgresql://childtrack_mumz_user:Ru3RZzMTRgz2pRgsiwJDz89qht8PNqbz@dpg-d4aoesvpm1nc739bk780-a/childtrack_mumz'
+            'postgresql://childtrack_5_user:jelQmbt20kDZLt9QTSrRvSOtiff2O46B@dpg-d4cqb1n5r7bs73aj421g-a/childtrack_5'
         )
     )
 }
