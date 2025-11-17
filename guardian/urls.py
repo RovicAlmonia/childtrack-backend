2340eef998243a4dbc49a08c276a896d6799f91a
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-17 16:02:02 +0800

    Update urls.py

--- guardian/urls.py
+++ guardian/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GuardianView
+from .views import GuardianView, GuardianByTeacherView
 
 app_name = 'guardian'
 
 urlpatterns = [
     path('', GuardianView.as_view(), name='guardian-list-create'),
     path('<int:pk>/', GuardianView.as_view(), name='guardian-detail'),
+    path('teacher/<int:teacher_id>/', GuardianByTeacherView.as_view(), name='guardian-by-teacher'),
 ]
