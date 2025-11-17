677363a9e236a3406b63f3a6ab3be1f5423edc89
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-17 13:25:08 +0800

    Update models.py

--- guardian/models.py
+++ guardian/models.py
@@ -2,18 +2,35 @@ from django.db import models
 from teacher.models import TeacherProfile
 
 class Guardian(models.Model):
-    teacher = models.ForeignKey(TeacherProfile, on_delete=models.CASCADE, related_name='guardians')
+    teacher = models.ForeignKey(
+        TeacherProfile, 
+        on_delete=models.CASCADE, 
+        related_name='guardians'
+    )
     name = models.CharField(max_length=255)
     age = models.IntegerField()
     address = models.TextField(blank=True, null=True)
     relationship = models.CharField(max_length=100, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=True, null=True)
     student_name = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='guardian_photos/', blank=True, null=True)  # NEW FIELD
+    photo = models.ImageField(
+        upload_to='guardian_photos/', 
+        blank=True, 
+        null=True,
+        help_text='Guardian photo'
+    )
     timestamp = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         ordering = ['-timestamp']
+        verbose_name = 'Guardian'
+        verbose_name_plural = 'Guardians'
     
     def __str__(self):
         return f"{self.name} - Guardian of {self.student_name}"
+    
+    def get_photo_url(self):
+        """Get the full URL for the photo"""
+        if self.photo:
+            return self.photo.url
+        return None
