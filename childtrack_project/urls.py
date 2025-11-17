5b2cf9e4f57ca278364781d3e3f589b1bf19dc2e
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-17 13:30:55 +0800

    Update urls.py

--- childtrack_project/urls.py
+++ childtrack_project/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('api/guardian/', include('guardian.urls')),
     path('api/parents/', include('parents.urls')),  # ✅ ADD THIS
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
