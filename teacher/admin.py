1ba795d6447c7599c3d45470f5f067efb648403e
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-18 12:20:56 +0800

    Update admin.py

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TeacherProfile, Guardian, Attendance, Absence, Dropout, UnauthorizedPerson
+from .models import TeacherProfile, Attendance, UnauthorizedPerson
 
 
 @admin.register(TeacherProfile)
@@ -9,49 +9,11 @@ class TeacherProfileAdmin(admin.ModelAdmin):
     list_filter = ['gender', 'section']
 
 
-@admin.register(Guardian)
-class GuardianAdmin(admin.ModelAdmin):
-    list_display = ['guardian_name', 'student_name', 'relation', 'contact', 'is_primary', 'teacher', 'timestamp']
-    search_fields = ['guardian_name', 'student_name', 'student_lrn', 'contact', 'teacher__user__username']
-    list_filter = ['relation', 'is_primary', 'teacher']
-    date_hierarchy = 'timestamp'
-    ordering = ['-is_primary', 'student_name', 'guardian_name']
-    fieldsets = (
-        ('Student Information', {
-            'fields': ('teacher', 'student_name', 'student_lrn')
-        }),
-        ('Guardian Information', {
-            'fields': ('guardian_name', 'relation', 'contact', 'email', 'address', 'occupation')
-        }),
-        ('Emergency & Additional', {
-            'fields': ('emergency_contact', 'photo', 'is_primary')
-        }),
-    )
-
-
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ['student_name', 'teacher', 'date', 'status', 'session', 'gender', 'timestamp']
-    search_fields = ['student_name', 'student_lrn', 'teacher__user__username']
+    list_display = ['student_name', 'guardian_name', 'teacher', 'date', 'status', 'session', 'gender', 'timestamp']
+    search_fields = ['student_name', 'student_lrn', 'guardian_name', 'teacher__user__username']
     list_filter = ['status', 'date', 'session', 'gender', 'teacher']
-    date_hierarchy = 'date'
-    ordering = ['-date', '-timestamp']
-
-
-@admin.register(Absence)
-class AbsenceAdmin(admin.ModelAdmin):
-    list_display = ['student_name', 'teacher', 'date', 'reason', 'timestamp']
-    search_fields = ['student_name', 'teacher__user__username']
-    list_filter = ['date', 'teacher']
-    date_hierarchy = 'date'
-    ordering = ['-date', '-timestamp']
-
-
-@admin.register(Dropout)
-class DropoutAdmin(admin.ModelAdmin):
-    list_display = ['student_name', 'teacher', 'date', 'reason', 'timestamp']
-    search_fields = ['student_name', 'teacher__user__username']
-    list_filter = ['date', 'teacher']
     date_hierarchy = 'date'
     ordering = ['-date', '-timestamp']
 
