6d6a5b93503f149780ca86cd2f045511d38210fc
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-18 14:59:28 +0800

    Update admin.py

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,22 +1,19 @@
 from django.contrib import admin
 from .models import TeacherProfile, Attendance, UnauthorizedPerson
 
-
 @admin.register(TeacherProfile)
 class TeacherProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'section', 'age', 'gender', 'contact']
     search_fields = ['user__username', 'section']
     list_filter = ['gender', 'section']
 
-
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ['student_name', 'guardian_name', 'teacher', 'date', 'status', 'session', 'gender', 'timestamp']
+    list_display = ['student_name', 'guardian_name', 'teacher', 'date', 'status', 'transaction_type', 'session', 'gender', 'timestamp']
     search_fields = ['student_name', 'student_lrn', 'guardian_name', 'teacher__user__username']
-    list_filter = ['status', 'date', 'session', 'gender', 'teacher']
+    list_filter = ['status', 'transaction_type', 'date', 'session', 'gender', 'teacher']
     date_hierarchy = 'date'
     ordering = ['-date', '-timestamp']
-
 
 @admin.register(UnauthorizedPerson)
 class UnauthorizedPersonAdmin(admin.ModelAdmin):
