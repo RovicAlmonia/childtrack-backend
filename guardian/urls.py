1d77e1752537dd5757cbbfa966446a8c6b5ff18a
JCBT04 <161135588+JCBT04@users.noreply.github.com>
2025-11-19 23:20:27 +0800

    guardian public view

--- guardian/urls.py
+++ guardian/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GuardianView, GuardianByTeacherView
+from .views import GuardianView, GuardianByTeacherView, GuardianPublicListView
 
 app_name = 'guardian'
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', GuardianView.as_view(), name='guardian-list-create'),
     path('<int:pk>/', GuardianView.as_view(), name='guardian-detail'),
     path('teacher/<int:teacher_id>/', GuardianByTeacherView.as_view(), name='guardian-by-teacher'),
+     # new
+    path('public/', GuardianPublicListView.as_view(), name='guardian-public-list'),
+
 ]
