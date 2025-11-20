ea89bdcf792839bf4b214d6c2305506739e500cb
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-20 12:06:10 +0800

    Update urls.py

--- guardian/urls.py
+++ guardian/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', GuardianView.as_view(), name='guardian-list-create'),
     path('<int:pk>/', GuardianView.as_view(), name='guardian-detail'),
     path('teacher/<int:teacher_id>/', GuardianByTeacherView.as_view(), name='guardian-by-teacher'),
-     # new
+
     path('public/', GuardianPublicListView.as_view(), name='guardian-public-list'),
 
 ]
