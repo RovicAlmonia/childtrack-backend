e26665e6f00724196795bfe1d5d0b018e722ed98
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-11-26 15:49:20 +0800

    Update urls.py

--- childtrack_project/urls.py
+++ childtrack_project/urls.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from parents.views import ParentNotificationListCreateView, ParentEventListCreateView, ParentScheduleListCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('teacher.urls')),
     path('api/guardian/', include('guardian.urls')),
     path('api/parents/', include('parents.urls')),  # ✅ ADD THIS
+    
+    path('api/notifications/', ParentNotificationListCreateView.as_view(), name='notifications'),
+    path('api/events/', ParentEventListCreateView.as_view(), name='events'),
+    path('api/schedule/', ParentScheduleListCreateView.as_view(), name='schedule'),
 ]
 
 if settings.DEBUG:
