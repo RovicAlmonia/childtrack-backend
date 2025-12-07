e85d4f9e693f98dfd9c05ccad099b3dee20f3442
jcbt <tevesjhunel01@gmail.com>
2025-12-07 20:06:48 +0800

    debug

--- parents/urls.py
+++ parents/urls.py
@@ -19,6 +19,8 @@ from .views import (
     ParentEventDetailView,
     ParentScheduleListCreateView,
     AvatarDebugView,
+    AvatarRedirectView,  # <-- ADD THIS IMPORT
+    AvatarDebugInfoView,  # <-- ADD THIS IMPORT
 )
 
 urlpatterns = [
@@ -54,9 +56,9 @@ urlpatterns = [
     
     # Schedules
     path('schedules/', ParentScheduleListCreateView.as_view(), name='schedule-list-create'),
-    # Debug endpoint to check uploaded avatar files (remove in production)
-    path('debug/avatar-exists/', AvatarDebugView.as_view(), name='avatar-debug'),
-    # Redirect endpoint to view a parent's avatar image
+    
+    # Avatar endpoints
     path('avatar/<int:pk>/', AvatarRedirectView.as_view(), name='avatar-redirect'),
     path('avatar-debug/<int:pk>/', AvatarDebugInfoView.as_view(), name='avatar-debug-info'),
+    path('debug/avatar-exists/', AvatarDebugView.as_view(), name='avatar-debug'),
 ]
