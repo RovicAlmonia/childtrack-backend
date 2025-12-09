5c6ca2a674204d9db6a64a60550b3f74a0e8c0ac
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-12-09 23:57:19 +0800

    Update models.py

--- teacher/models.py
+++ teacher/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    section = models.CharField(max_length=50)
+    contact = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
 
 class ScanPhoto(models.Model):
     """Store photos captured during attendance scans"""
