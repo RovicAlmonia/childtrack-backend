09748265509e69a35e784ea9c65c088954158f1e
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-12-09 23:34:04 +0800

    Update models.py

--- teacher/models.py
+++ teacher/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .models import TeacherProfile
 
 class ScanPhoto(models.Model):
     """Store photos captured during attendance scans"""
